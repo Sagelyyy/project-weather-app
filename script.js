2b194d0425474285caaccbf5f63b8a4e3f0c024d
Chris <orj.tsuk@gmail.com>
2021-09-14 22:02:29 -0400

    Finished up some more styling, added support for night icons.

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ const error = "Please enter a valid city.";
 const myCity = {};
 
 cityInput.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      processData(cityInput.value);
-    }
-  });
+  if (event.keyCode === 13) {
+    processData(cityInput.value);
+  }
+});
 
+function pageLoadTime() {
+  const timeText = document.querySelector(".timer");
+  const perfData = window.performance.timing;
+  const connectTime = perfData.responseEnd - perfData.requestStart + "ms";
+  timeText.textContent = `Page loaded in ${connectTime}`;
+}
 
 async function getCityData(cityName) {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
   console.log(url);
   if (cityName != "") {
     try {
+      pageLoadTime();
       const response = await fetch(url, { mode: "cors" });
       cityInput.value = "";
       return (cityData = await response.json());
@@ -51,43 +58,133 @@ async function processData(cityName) {
 
 function selectWeatherImage() {
   const svg = document.querySelector("#weather");
-  switch (myCity.weather) {
-    case "clear sky":
-      svg.className = "wi wi-day-sunny";
-      svg.style.color = 'yellow'
-      break;
-    case "few clouds":
-      svg.className = "wi wi-day-cloudy";
-      svg.style.color = 'gray'
-      break;
-    case "scattered clouds":
-      svg.className = "wi wi-day-cloudy";
-      svg.style.color = 'gray'
-      break;
-    case "broken clouds":
-      svg.className = "wi wi-day-cloudy";
-      svg.style.color = 'gray'
-      break;
-    case "shower rain":
-      svg.className = "wi wi-day-sprinkle";
-      svg.style.color = 'gray'
-      break;
-    case "rain":
-      svg.className = "wi wi-day-rain";
-      svg.style.color = 'gray'
-      break;
-    case "thunderstorm":
-      svg.className = "wi wi-day-thunderstorm";
-      svg.style.color = 'gray'
-      break;
-    case "snow":
-      svg.className = "wi wi-day-snow";
-      svg.style.color = 'white'
-      break;
-    case "mist":
-      svg.className = "wi wi-day-rain";
-      svg.style.color = 'gray'
-      break;
+  let currAmPm = myCity.asOf.slice(-2);
+  if (currAmPm == "AM") {
+    switch (myCity.weather) {
+      case "clear sky":
+        svg.className = "wi wi-day-sunny";
+        svg.style.color = "yellow";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #0093E9 0%, #80D0C7 100%)";
+        break;
+      case "few clouds":
+        svg.className = "wi wi-day-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #accbee 0%, #e7f0fd 100%)";
+        break;
+      case "scattered clouds":
+        svg.className = "wi wi-day-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #6a85b6 0%, #bac8e0 100%)";
+        break;
+      case "broken clouds":
+        svg.className = "wi wi-day-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #d9afd9 0%, #97d9e1 100%)";
+        break;
+      case "overcast clouds":
+        svg.className = "wi wi-day-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #d9afd9 0%, #97d9e1 100%)";
+        break;
+      case "shower rain":
+        svg.className = "wi wi-day-sprinkle";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #74ebd5 0%, #9face6 100%)";
+        break;
+      case "rain":
+        svg.className = "wi wi-day-rain";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #13547a 0%, #80d0c7 100%)";
+        break;
+      case "thunderstorm":
+        svg.className = "wi wi-day-thunderstorm";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #c1c161 0%, #c1c161 0%, #d4d4b1 100%)";
+        break;
+      case "snow":
+        svg.className = "wi wi-day-snow";
+        svg.style.color = "white";
+        document.body.style.backgroundImage =
+          "linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)";
+        break;
+      case "mist":
+        svg.className = "wi wi-day-rain";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #74ebd5 0%, #9face6 100%)";
+        break;
+    }
+  } else {
+    switch (myCity.weather) {
+      case "clear sky":
+        svg.className = "wi wi-night-clear";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #0093E9 0%, #80D0C7 100%)";
+        break;
+      case "few clouds":
+        svg.className = "wi wi-night-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #accbee 0%, #e7f0fd 100%)";
+        break;
+      case "scattered clouds":
+        svg.className = "wi wi-night-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #6a85b6 0%, #bac8e0 100%)";
+        break;
+      case "broken clouds":
+        svg.className = "wi wi-night-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #d9afd9 0%, #97d9e1 100%)";
+        break;
+      case "overcast clouds":
+        svg.className = "wi wi-night-cloudy";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #d9afd9 0%, #97d9e1 100%)";
+        break;
+      case "shower rain":
+        svg.className = "wi wi-night-sprinkle";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #74ebd5 0%, #9face6 100%)";
+        break;
+      case "rain":
+        svg.className = "wi wi-night-rain";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #13547a 0%, #80d0c7 100%)";
+        break;
+      case "thunderstorm":
+        svg.className = "wi wi-night-thunderstorm";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #c1c161 0%, #c1c161 0%, #d4d4b1 100%)";
+        break;
+      case "snow":
+        svg.className = "wi wi-night-snow";
+        svg.style.color = "white";
+        document.body.style.backgroundImage =
+          "linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)";
+        break;
+      case "mist":
+        svg.className = "wi wi-night-rain";
+        svg.style.color = "gray";
+        document.body.style.backgroundImage =
+          "linear-gradient(62deg, #74ebd5 0%, #9face6 100%)";
+        break;
+    }
   }
 }
 
@@ -121,10 +218,12 @@ function displayData() {
   const tempDisplay = document.querySelector(".temp");
   const descImgDisplay = document.querySelector(".desc-img");
   const asOfTime = document.querySelector(".time");
+  const tempDesc = document.querySelector('.temp-desc')
 
   cityDisplay.textContent = myCity.name;
   tempDisplay.textContent = `${Math.round(myCity.temp)}°`;
   asOfTime.textContent = `As of ${myCity.asOf}`;
+  tempDesc.textContent = myCity.weather
 }
 
 citySubmit.addEventListener("click", (e) => {
